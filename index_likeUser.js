6daec15a3795e86d1e37c036afec694be7a75e03
wildmeng <60999287+mengwild@users.noreply.github.com>
2026-01-20 10:37:38 +0800

    Update index_likeUser.js

--- index_likeUser.js
+++ index_likeUser.js
@@ -308,7 +308,7 @@
   userInput.style.borderRadius = "8px";
   userInput.style.backgroundColor = "#e0e0e0";
   userInput.style.width = "100px";
-  userInput.value = localStorage.getItem("specificUser") || "14790897";
+  userInput.value = localStorage.getItem("specificUser") || "";
 
   document.body.appendChild(userInput);
 
